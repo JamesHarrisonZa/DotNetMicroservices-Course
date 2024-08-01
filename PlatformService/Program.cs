17264226b9503ce2a77107179a8e313b70535e2d
JamesH <James.Harrison.Za@gmail.com>
2024-08-01 16:07:21 +1200

    Initial commit. Add PlatformService and first Model + DbContext

---
+++ PlatformService/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,5 @@
+# .NET Microservices – Full Course
+
+## Reference
+
+https://www.youtube.com/watch?v=DgVjEo3OGBI&list=PLpXfHEl2fzl7a7p4ntTmdjmNSD1iEYXrm
