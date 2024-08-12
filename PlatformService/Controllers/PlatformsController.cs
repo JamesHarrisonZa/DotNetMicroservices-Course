a53d14f4b5ad01f2e3a1fa3623dafe58b3581c8c
JamesH <James.Harrison.Za@gmail.com>
2024-08-12 17:02:06 +1200

    Add endpoints for GetPlatformById and CreatePlatform

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
@@ -28,5 +29,28 @@ public class PlatformsController : ControllerBase
         var platformReadDtos = _mapper.Map<IEnumerable<PlatformReadDto>>(platformItem);
 
         return Ok(platformReadDtos);
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        var platformItem = _repository.GetPlatformById(id);
+        if (platformItem == null) return NotFound();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformItem);
+        
+        return Ok(platformReadDto);
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto .Id }, platformReadDto); // Return status 201 and the route you can get the resource as response header
     }
 }
